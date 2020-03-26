9a35545249be5592a3141504ee9476bed8f79a62
David Strootman <david.strootman@gmail.com>
2020-03-26 21:09:34 +0100

    Image copying

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -1,5 +1,6 @@
 #include "StudentPreProcessing.h"
 
+#include <algorithm>
 #include <math.h>
 #include <stdlib.h>
 
@@ -19,11 +20,11 @@ IntensityImage* StudentPreProcessing::stepEdgeDetection(const IntensityImage& im
 	gaussianBlur(image, *edgeDetectResultImage);
 
 	// Step 2 - Sobel Edge Detector
-	//IntensityImage* sobelOutput = ImageFactory::newIntensityImage(image.getWidth(), image.getHeight());
-	//sobelEdgeDetector(*edgeDetectResultImage, *sobelOutput);
+	IntensityImage* sobelOutput = ImageFactory::newIntensityImage(image.getWidth(), image.getHeight());
+	sobelEdgeDetector(*edgeDetectResultImage, *sobelOutput);
 
 	// Step 3 - Non-Maximum Suppression
-	return edgeDetectResultImage;
+	return sobelOutput;
 }
 
 IntensityImage* StudentPreProcessing::stepThresholding(const IntensityImage& image) const {
@@ -45,10 +46,11 @@ IntensityImage* StudentPreProcessing::stepThresholding(const IntensityImage& ima
 
 void StudentPreProcessing::gaussianBlur(const IntensityImage& image, IntensityImage& output) const
 {
-	constexpr int data[] = { 1, 2, 1,
+	constexpr int kernel[] = { 1, 2, 1,
 							 2, 4, 2,
 							 1, 2, 1 };
-	convolution(image, output, data);
+
+	convolution(image, output, kernel);
 }
 
 void StudentPreProcessing::sobelEdgeDetector(const IntensityImage& image, IntensityImage& output) const
@@ -65,12 +67,13 @@ void StudentPreProcessing::sobelEdgeDetector(const IntensityImage& image, Intens
 	IntensityImage* xFilterOutput = ImageFactory::newIntensityImage(image.getWidth(), image.getHeight());
 	IntensityImage* yFilterOutput = ImageFactory::newIntensityImage(image.getWidth(), image.getHeight());
 
-	convolution(image, output, xFilter);
-	//convolution(image, *yFilterOutput, yFilter);
+	convolution(image, *xFilterOutput, xFilter);
+	convolution(image, *yFilterOutput, yFilter);
 
 	for (size_t i = 0; i < 0; i++)
 	{
-		//output.setPixel(i, sqrt(pow(xFilterOutput->getPixel(i), 2) + pow(yFilterOutput->getPixel(i), 2)));
+		int sobelPixel = sqrt(pow(xFilterOutput->getPixel(i), 2) + pow(yFilterOutput->getPixel(i), 2));
+		output.setPixel(i, std::min(sobelPixel, 255));
 	}
 
 	// sqrt(pow(255, 2) + pow(255, 2)) = 360 :monkers:
@@ -84,22 +87,27 @@ void StudentPreProcessing::convolution(const IntensityImage& image, IntensityIma
 		kernelTotal += abs(kernel[i]);
 	}
 
-	// TODO: Bounds checking
-	for (int y = 1; y < image.getHeight() - 1; y++)
+	// TODO: Optimize this to only copy the outer edge.
+	for (int i = 0; i < image.getWidth() * image.getHeight(); i++)
 	{
-		for (int x = 1; x < image.getWidth() - 1; x++)
+		output.setPixel(i, image.getPixel(i));
+	}
+
+	for (int y = 1; y < image.getHeight(); y++)
+	{
+		for (int x = 1; x < image.getWidth(); x++)
 		{
-			int topLeftPixel = image.getPixel(x - 1, y - 1) * kernel[0];
-			int topCenterPixel = image.getPixel(x, y - 1) * kernel[1];
-			int topRightPixel = image.getPixel(x + 1, y - 1) * kernel[2];
+			int topLeftPixel = image.getPixel(x-1, y-1) * kernel[0];
+			int topCenterPixel = image.getPixel(x, y-1) * kernel[1];
+			int topRightPixel = image.getPixel(x+1,y-1) * kernel[2];
 
-			int middleLeftPixel = image.getPixel(x - 1, y) * kernel[3];
+			int middleLeftPixel = image.getPixel(x-1, y) * kernel[3];
 			int middleCenterPixel = image.getPixel(x, y) * kernel[4];
-			int middleRightPixel = image.getPixel(x + 1, y) * kernel[5];
+			int middleRightPixel = image.getPixel(x+1,y) * kernel[5];
 
-			int bottomLeftPixel = image.getPixel(x - 1, y + 1) * kernel[6];
-			int bottomCenterPixel = image.getPixel(x, y + 1) * kernel[7];
-			int bottomRightPixel = image.getPixel(x + 1, y + 1) * kernel[8];
+			int bottomLeftPixel = image.getPixel(x- 1,y+1) * kernel[6];
+			int bottomCenterPixel = image.getPixel(x, y+1) * kernel[7];
+			int bottomRightPixel = image.getPixel(x+1, y+1) * kernel[8];
 
 			int newValue = topLeftPixel + topCenterPixel + topRightPixel + middleLeftPixel + middleCenterPixel + middleRightPixel + bottomLeftPixel + bottomCenterPixel + bottomRightPixel;
 			output.setPixel(x, y, newValue / kernelTotal);
